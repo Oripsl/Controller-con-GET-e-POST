9fedd39bb06507f956700d6ffc842ec8c1a5b418
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-04-08 16:27:42 +0200

    sis

--- src/main/java/com/example/demoex/NameReverseController.java
+++ src/main/java/com/example/demoex/NameReverseController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-public class NameReverse {
+@RequestMapping("/v1")
+public class NameReverseController {
     @GetMapping("/reverse/{name}")
     public String returnName(@PathVariable String name){
         return name;
@@ -12,6 +13,7 @@ public class NameReverse {
 
     @PostMapping("/reverse")
     public String reverseName(@RequestBody String name) {
+
         return new StringBuilder(name).reverse().toString();
     }
 }
